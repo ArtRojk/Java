007d965f96674bc5856f7fa670b132791669da3b
ArtRojk <87004812+ArtRojk@users.noreply.github.com>
2022-02-04 16:51:45 +0300

    Add files via upload

--- HomeWorkApp.java
+++ HomeWorkApp.java
@@ -11,35 +11,36 @@ import java.util.Arrays;
 class HomeWorkApp {
 
     public static void main(String[] args) {
+
         entry(5, 5);
 
         int[] arr1 = {1, 0, 1, 0, 0, 1};
         System.out.println(Arrays.toString(arr1));
         for (int i = 0; i < arr1.length; i++){
-        arr1[i] = 1 - arr1[i];
+            arr1[i] = 1 - arr1[i];
         }
         System.out.println(Arrays.toString(arr1));
 
         int[] arr2 = new int[100];
-        arr2[0] = 0;
+            arr2[0] = 0;
         System.out.println(Arrays.toString(arr2));
         for (int i = 1; i < arr2.length; i++){
-        arr2[i] = i + 1;
+            arr2[i] = i + 1;
         System.out.println(Arrays.toString(arr2));
         }
 
         int[] arr3 = {1, 5, 3, 2, 11, 4, 5, 2, 4, 8, 9, 1};
         System.out.println(Arrays.toString(arr3));
         for (int i = 0; i < arr3.length; i++) {
-        if (arr3[i] < 6)
-        arr3[i] *= 2;
+            if (arr3[i] < 6)
+            arr3[i] *= 2;
         }
         System.out.println(Arrays.toString(arr3));
 
         int[][] arr4 = new int[5][5];
         for (int i = 0; i < arr4.length; i++){
-        arr4[i][i] = 1;
-        arr4[i][arr4.length - i - 1] = 1;
+            arr4[i][i] = 1;
+            arr4[i][arr4.length - i - 1] = 1;
         }
         System.out.println(Arrays.toString(arr4));
         for (int i = 0; i < arr4.length; i++) {
@@ -47,12 +48,12 @@ class HomeWorkApp {
         }
         }
 
-    static void entry(int len, int initialValue) {
+    static int[] entry(int len, int initialValue) {
         int arr5[] = new int[len];
         for (int i = 0; i < arr5.length; i++) {
-        arr5[i] = initialValue;
+            arr5[i] = initialValue;
         }
-        System.out.println(Arrays.toString(arr5));
+        return arr5;
     }
 }
 
